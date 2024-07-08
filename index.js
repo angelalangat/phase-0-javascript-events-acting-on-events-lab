d68c36a0c0423e775d251138de9f4bac6e069875
angie <angelaclangat@gmai.com>
2024-07-08 16:31:32 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+
+//build moveDodgerLeft() function
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+//wire it up to event listener
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+
+//function moveDodgerRight
+function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers,10);
+
+    if (left > 0){
+        dodger.style.left = `${left + 1}px`;
+    
+    }
+}
+
+//event listiner
+document.addEventListener("keydown", function (e){
+    if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+});
+
